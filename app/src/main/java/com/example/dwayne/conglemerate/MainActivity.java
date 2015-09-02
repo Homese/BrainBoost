3f6fd4abc99cc3c7221ee0d27d5a09d0f1554958
Dwayne Sauls <dwayneuni@gmail.com>
2015-09-02 09:07:42 +0200

    BB

--- app/src/main/java/com/example/dwayne/conglemerate/MainActivity.java
+++ app/src/main/java/com/example/dwayne/conglemerate/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.dwayne.conglemerate;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -7,29 +8,39 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
     }
-    public void switchFRagments(View view) {
+public void results(View v){
+    FragmentManager fragmentManager = getSupportFragmentManager();
+    Fragment fragmentA = new Numbers();
+    fragmentManager.beginTransaction()
+            .replace(R.id.mainContent, fragmentA)
+            .addToBackStack(null)
+            .commit();
+}
+    public void wrongchoice(View v){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragmentA = new Numbers();
+        fragmentManager.beginTransaction()
+                .replace(R.id.mainContent, fragmentA)
+                .addToBackStack(null)
+                .commit();
+    }
+    public void click(View view) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         Fragment fragmentA = new Exercises();
         fragmentManager.beginTransaction()
                 .replace(R.id.mainContent, fragmentA)
                 .addToBackStack(null)
                 .commit();
-    }
-    public void click(View v) {
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        Fragment fragmentA = new Numbers();
-        fragmentManager.beginTransaction().hide(fragmentA)
-                .replace(R.id.mainContent, fragmentA)
-                .commit();
-        Intent intent = new Intent(this.getApplicationContext(),Numbers.class);
-        startActivity(intent);
+
     }
 }
