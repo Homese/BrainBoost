fdc479f39b1a782f749228d48d796b32f91d38e3
Dwayne Sauls <dwayneuni@gmail.com>
2015-09-04 10:15:25 +0200

    Editing

--- app/src/main/java/com/example/dwayne/conglemerate/Celebration.java
+++ app/src/main/java/com/example/dwayne/conglemerate/Celebration.java
@@ -2,11 +2,16 @@ package com.example.dwayne.conglemerate;
 
 
 
+import android.content.Context;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 
 public class Celebration extends Fragment {
@@ -17,8 +22,8 @@ public class Celebration extends Fragment {
         //Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_celebration, container, false);
 
-
     }
+
 
 
 
